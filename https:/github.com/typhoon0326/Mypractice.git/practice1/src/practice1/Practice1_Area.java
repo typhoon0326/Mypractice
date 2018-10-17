dddce4f296bca6dcef20bba24905732d5ddd0157
佩璇 <peixuanjian@gmail.com>
2018-10-17 23:10:13 +0800

    加法練習

---
+++ https:/github.com/typhoon0326/Mypractice.git/practice1/src/practice1/Practice1_Area.java
@@ -0,0 +1,23 @@
+package practice1;
+
+public class Practice1_Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//常數宣告
+		final double PI = 3.1415926;
+	
+		//變數宣告
+		double area;
+	    double r = 20.0;
+	    
+	    //計算面積
+	    area = PI*r*r;
+	    
+	    System.out.print("圓面積");
+	    System.out.println(area);
+
+	}
+
+}
